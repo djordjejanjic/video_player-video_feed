a17cd39986a866a03bc391caf631540efade9f27
djord <djord@DESKTOP-EA378QQ>
2020-08-08 13:33:48 +0200

    Sredjivanje koda i novo dodavanje funkcionalnosti

--- Source.cpp
+++ Source.cpp
@@ -8,13 +8,25 @@
 //Funkcija za biranje da li korisnik zeli da pokrene video ili web kameru
 int prompt() {
 	int x;
-	
+
 	do {
 		printf("Da li zelite da pokrenete video ili web cameru (1 - video, 0 - web camera): ");
 		scanf("%d", &x);
-	} while (getchar() != '\n' && x != 0 && x != 1);
+	} while (x < 0 || x > 1);
+	getchar();
 
 	return x;
+}
+
+char* user() {
+	char user[20];
+
+	printf("Upisite Vas user_name (ex: my user_name is djord in C:\\Users\\djord\\): ");
+	scanf("%[^\n]", user);
+	getchar();
+	printf("\n");
+
+	return user;
 }
 
 //Funkcija za biranje videa koji korisnik zeli da pokrene
@@ -23,23 +35,24 @@ char* browse() {
 	
 	printf("Upisite lokaciju Vaseg videa | ex: video.mp4 (video se mora nalaziti na dekstopu): ");
 	scanf("%[^\n]", string);
+	getchar();
 	printf("\n");
 	printf("Otvorili ste video: %s\n\n", string);
 	
 	return string;
 }
 
-int main()
-{
+int generateVideo() {
 	int option = prompt();
 	char string[30];
-	char string1[100]= "C:\\Users\\djord\\Desktop\\";
-	
+	char string1[100] = "C:\\Users\\";
+	char *location = "\\Desktop\\";
+
 	printf("\n");
 
 	//Kreiranje objekta za video
 	if (option == 0) {
-		
+
 		cv::VideoCapture video(0);
 
 		//Proveravanje da li je video otvoren
@@ -59,7 +72,7 @@ int main()
 		while (video.read(frejmovi))
 		{
 			//Prikaz frejmova
-			cv::imshow("Video player in C++", frejmovi);
+			cv::imshow("Web_cam in C++", frejmovi);
 
 			//Ispisi frejm
 			output.write(frejmovi);
@@ -73,8 +86,12 @@ int main()
 		output.release();
 		video.release();
 	}
-	else 
+	else
 	{
+		//Spajanje string1 promenljive sa upisanim korisnickim imenom od strane korisnika i onda spajanje Desktop lokacije sa datim korisnickim imenom
+		strcat(string1, user());
+		strcat(string1, location);
+
 		//Kopiranje stringa iz funkcije browse() u lokalni string
 		strncpy(string, browse(), 30);
 
@@ -82,12 +99,13 @@ int main()
 		strcat(string1, string);
 
 		cv::VideoCapture video(string1);
-		
-		int sirina = video.get(cv::CAP_PROP_FRAME_WIDTH);
-		int visina = video.get(cv::CAP_PROP_FRAME_HEIGHT);
 
-		video.set(cv::CAP_PROP_FRAME_WIDTH, sirina/2);
-		video.set(cv::CAP_PROP_FRAME_HEIGHT, visina/2);
+		double sirina = video.get(cv::CAP_PROP_FRAME_WIDTH);
+		double visina = video.get(cv::CAP_PROP_FRAME_HEIGHT);
+
+		//Postavljanje sirine i visine videa tako da visina dolazi do ivica ekrana, a sirina respektivno smanjena u odnosu na razmeru width X height
+		video.set(cv::CAP_PROP_FRAME_WIDTH, 800 * (sirina / visina));
+		video.set(cv::CAP_PROP_FRAME_HEIGHT, 800);
 
 		//Proveravanje da li je video otvoren
 		if (!video.isOpened()) return -1;
@@ -97,8 +115,8 @@ int main()
 
 		while (video.read(frejmovi))
 		{
-			resize(frejmovi, frejmovi, cv::Size(sirina/2, visina/2), 0, 0, cv::INTER_CUBIC);
-			cv::namedWindow("Video player in C++", 1);
+			resize(frejmovi, frejmovi, cv::Size(800 * (sirina / visina), 800), 0, 0, cv::INTER_CUBIC);
+			cv::namedWindow("Video player in C++", cv::WINDOW_AUTOSIZE);
 			//Prikaz frejmova
 			cv::imshow("Video player in C++", frejmovi);
 
@@ -109,9 +127,15 @@ int main()
 			}
 		}
 		video.release();
-	}	
-
+	}
 	cv::destroyAllWindows();
+	
+	return 0;
+}
+
+int main()
+{
+	generateVideo();
 
 	return 0;
 }
